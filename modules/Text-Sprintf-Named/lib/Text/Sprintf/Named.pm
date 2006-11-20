09247d078ac73b742aa32f8bbe67dc5b4c252033
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-11-20 15:57:44 +0000

    Importing the Text-Sprintf-Named files
    git-svn-id: https://svn.berlios.de/svnroot/repos/web-cpan/Text-Sprintf/trunk@1626 52c325ad-5fd0-0310-8a0f-c43feede02cc

---
+++ modules/Text-Sprintf-Named/lib/Text/Sprintf/Named.pm
@@ -0,0 +1,100 @@
+package Text::Sprintf::Named;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Text::Sprintf::Named - The great new Text::Sprintf::Named!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Text::Sprintf::Named;
+
+    my $foo = Text::Sprintf::Named->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-text-sprintf-named at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Text::Sprintf::Named>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Text::Sprintf::Named
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Text::Sprintf::Named>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Text::Sprintf::Named>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Text::Sprintf::Named>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Text::Sprintf::Named>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: bsd
+
+=cut
+
+1; # End of Text::Sprintf::Named
