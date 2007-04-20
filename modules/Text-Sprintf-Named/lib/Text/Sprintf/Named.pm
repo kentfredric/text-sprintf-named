91bb7a7d7f7e4c6947d20c5a8d9d4124364ec06f
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-04-20 06:23:25 +0000

    Documented the change and bumped the version number.
    git-svn-id: https://svn.berlios.de/svnroot/repos/web-cpan/Text-Sprintf/trunk@1940 52c325ad-5fd0-0310-8a0f-c43feede02cc

--- modules/Text-Sprintf-Named/lib/Text/Sprintf/Named.pm
+++ modules/Text-Sprintf-Named/lib/Text/Sprintf/Named.pm
@@ -11,11 +11,11 @@ Text::Sprintf::Named - sprintf-like function with named conversions
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
